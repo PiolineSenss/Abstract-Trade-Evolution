4664cc58e17bfd5b3d49d92b51275c656bc377db
Игорь <igorsm982@gmail.com>
2026-04-23 22:33:35 +0300

    Шаблонизация

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from flask import Flask, send_from_directory
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, make_response
 from flask_cors import CORS
 from routers.auth import auth_bp
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
 
+app.config['JSON_AS_ASCII'] = False       # чтобы русский в JSON не убегал
+app.jinja_env.encoding = 'utf-8'          # принудительно для шаблонов
+
 app.register_blueprint(auth_bp)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('static', 'index.html')
+    resp = make_response(render_template('index.html'))
+    resp.headers['Content-Type'] = 'text/html; charset=utf-8'
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
