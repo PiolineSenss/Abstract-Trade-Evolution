b52fcd4f883b8bf6eff1c7835a02848c29890551
Игорь <igorsm982@gmail.com>
2026-04-24 00:25:20 +0300

    Кнопка ЛК с небольшим функционалом

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, render_template, make_response
 from flask_cors import CORS
 from routers.auth import auth_bp
@@ -6,8 +5,8 @@ from routers.auth import auth_bp
 app = Flask(__name__, static_folder='static')
 CORS(app)
 
-app.config['JSON_AS_ASCII'] = False       # чтобы русский в JSON не убегал
-app.jinja_env.encoding = 'utf-8'          # принудительно для шаблонов
+app.config['JSON_AS_ASCII'] = False
+app.jinja_env.encoding = 'utf-8'
 
 app.register_blueprint(auth_bp)
 
@@ -16,6 +15,10 @@ def serve_index():
     resp = make_response(render_template('index.html'))
     resp.headers['Content-Type'] = 'text/html; charset=utf-8'
     return resp
+
+@app.route('/profile')
+def serve_profile():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
